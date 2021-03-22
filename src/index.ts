d453324d6ddbee34269fd65ae7f6443c45994980
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-03-22 22:09:19 +0200

    updated config

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-import { Command } from 'commander';
-const program = new Command();
-program.version('0.0.1');
+#!/usr/bin/env node
+
+import program from 'commander';
+
+console.clear();
+program
+  .version('0.0.1')
+  .description('Convert you next.js project to typescript in minutes!')
+  .option('-h,--help', 'Help message')
+  .parse(process.argv);
