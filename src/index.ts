3c60f9a46c73ff24cd4efcf657cf2b263e5a5d4f
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-03-23 16:23:50 +0200

    banner done

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 #!/usr/bin/env node
-
 import program from 'commander';
+import chalk from 'chalk';
+import figlet from 'figlet';
 
-console.clear();
 program
   .version('0.0.1')
   .description('Convert you next.js project to typescript in minutes!')
   .option('-h,--help', 'Help message')
   .parse(process.argv);
+
+console.clear();
+console.log(
+  chalk.blueBright.bold(
+    figlet.textSync('next-ts', { horizontalLayout: 'full' })
+  )
+);
