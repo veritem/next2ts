54d14d605e4d04cb6d590236360725ab19b8f9cf
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-03-23 16:28:28 +0200

    test working

--- src/index.ts
+++ src/index.ts
@@ -15,3 +15,13 @@ console.log(
     figlet.textSync('next-ts', { horizontalLayout: 'full' })
   )
 );
+
+program
+  .version('0.0.1')
+  .description("An example CLI for ordering pizza's")
+  .option('-p, --peppers', 'Add peppers')
+  .option('-P, --pineapple', 'Add pineapple')
+  .option('-b, --bbq', 'Add bbq sauce')
+  .option('-c, --cheese <type>', 'Add the specified type of cheese [marble]')
+  .option('-C, --no-cheese', 'You do not want any cheese')
+  .parse(process.argv);
