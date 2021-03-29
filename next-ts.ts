ac775f08aa47438f48f29d3c2cc22b3c3541a38f
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-03-29 14:51:32 +0200

    fixed

--- next-ts.ts
+++ next-ts.ts
@@ -4,16 +4,22 @@ import { HandleInstallPackages } from './helpers/install';
 import chalk from 'chalk';
 import { FolderExists } from './helpers/folder-exists';
 import { join } from 'path';
+import path from 'path';
 
 export async function setupProject() {
+  console.log(join(__dirname, 'pages'));
+
   let pagesExists = await FolderExists(join(__dirname, 'pages'));
   let srcPageExists = await FolderExists(join(__dirname, 'src/pages'));
 
+  console.log('PageExists ' + pagesExists);
+
+  const baseName = path.basename(__dirname);
+
   if (!pagesExists || !srcPageExists) {
-    console.clear();
     console.log('\n');
     console.log(chalk.yellowBright('Next2ts\n'));
-    console.log(chalk.red(`This is not a Next.js Project!`));
+    console.log(chalk.red(`${baseName} is not a Next.js Project!`));
     process.exit(0);
   }
 
