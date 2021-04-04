bb8dcc48f565bf4a7b36ad280fe04ff376fb0994
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-04-04 14:48:18 +0200

    feat: progressing

--- index.js
+++ index.js
@@ -25,12 +25,12 @@ function hasYarn() {
 }
 
 async function init() {
-  console.log();
-
   const baseName = path.basename(__dirname);
 
-  let pagesExists = await folderExists(join(__dirname, 'pages'));
-  let pagesExistsInSrc = await folderExists(join(__dirname, 'src/pages'));
+  let pagesExists = await folderExists(join(__dirname, 'pages/index.js'));
+  let pagesExistsInSrc = await folderExists(
+    join(__dirname, 'src/pages/index.js')
+  );
 
   if (!pagesExists && !pagesExistsInSrc) {
     console.log('\n');
@@ -38,12 +38,40 @@ async function init() {
     console.log(chalk.red(`${baseName} is not a Next.js Project!`));
     process.exit(0);
   }
+
   if (hasYarn()) {
-    execSync('yarn add --dev typescript @types/react @types/node');
+    //execSync('yarn add --dev typescript @types/react @types/node');
   } else {
-    execSync('npm install -D typescript @types/react @types/node');
+    //execSync('npm install -D typescript @types/react @types/node');
   }
-  execSync('touch tsconfig.json');
+  //execSync('touch tsconfig.json');
+
+  fs.writeFile(
+    'tsconfig.json',
+    `
+{
+  "target": "es5",
+  "lib": ["dom", "dom.iterable", "esnext"],
+  "allowJs": true,
+  "skipLibCheck": true,
+  "strict": false,
+  "forceConsistentCasingInFileNames": true,
+  "noEmit": true,
+  "esModuleInterop": true,
+  "module": "esnext",
+  "moduleResolution": "node",
+  "resolveJsonModule": true,
+  "isolatedModules": true
+}
+    `,
+    function (e) {
+      if (e) throw e;
+      console.log(chalk.blue(`Added tsconfig.json in the root`));
+    }
+  );
+
+  //TODO: touch tsconfig.json
+  //TODO: Rename file and folders to tsx
 }
 
 init().catch((e) => {
