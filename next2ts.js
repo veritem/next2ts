b64892bef7b50a5ca54528e41215e339cbca5226
veritem <mugaboverite@gmail.com>
2021-05-03 12:30:21 +0200

    feat: renmaing files done

--- next2ts.js
+++ next2ts.js
@@ -1,55 +1,46 @@
 // https://gist.github.com/scriptex/20536d8cda36221f91d69a6bd4a528b3
 
-//TODO: check for the project page directory
-//TODO: Rename all files in the tree
-//TODO: Run npm install
+//TODO: (done) check for the project page directory
+//TODO: (done) Rename all files in the tree
+//TODO: Run  install
 
 import spawn from 'cross-spawn';
-import fs from 'fs';
+import fs, { renameSync } from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-function folderExists(path) {
-  return new Promise((resolve, reject) => {
-    access(path, function (err) {
-      if (err) {
-        if (err.code === 'ENOENT') {
-          return resolve(false);
-        }
-        return reject(err);
-      }
-      return resolve(true);
-    });
-  });
-}
+const cwd = process.cwd();
 
 function hasYarn() {
-  return fs.existsSync(path.resolve(process.cwd(), 'yarn.lock'));
+  return fs.existsSync(cwd, 'yarn.lock');
 }
 
 export async function init() {
-  const root = path.basename(__dirname);
+  const root = path.basename(cwd);
 
   console.log(`\nMigrating ${root} to typescript...`);
 
   if (
-    !folderExists(path.join(__dirname + '/pages')) ||
-    !folderExists(path.join(__dirname + '/src/pages'))
+    !(
+      fs.existsSync(path.join(cwd + '/pages')) ||
+      fs.existsSync(path.join(cwd + '/src/pages'))
+    )
   ) {
-    throw new Error('This is not a next.js project');
+    console.log(`\n${root} is not a next.js project`);
+    process.exit(1);
   }
 
-  let projectSource = folderExists(path.join(__dirname + '/pages'))
-    ? path.join(__dirname + '/pages')
-    : path.join(__dirname + '/src/pages');
+  let projectSource = fs.existsSync(path.join(cwd + '/pages'))
+    ? path.join(cwd + '/pages')
+    : path.join(cwd + '/src/pages');
 
   try {
     fs.copyFileSync(
-      path.resolve('template/tsconfig.json'),
-      __dirname + '/tsconfig.json'
+      path.join(__dirname + '/template/tsconfig.json'),
+      path.join(cwd + '/tsconfig.json')
     );
     console.log('\nInitilized tsconfig.json');
   } catch (e) {
@@ -57,17 +48,57 @@ export async function init() {
     process.exit(1);
   }
 
-  const allDependancies = ['typescript', '@types/react', '@types/node'];
+  renameFiles(projectSource);
 
-  return install(root, hasYarn(), allDependancies).then(() => {
-    console.log('Installing...............');
-  });
+  //TODO:
+
+  // const allDependancies = ['typescript', '@types/react', '@types/node'];
+
+  // return install(root, hasYarn(), allDependancies).then(() => {
+  //   console.log('Installing...............');
+  // });
+}
+
+function renameFiles(source) {
+  let files = [];
+
+  const getFilesRecursively = (directory) => {
+    const filesInDirectory = fs.readdirSync(directory);
+    for (const file of filesInDirectory) {
+      const absolute = path.join(directory, file);
+      if (fs.statSync(absolute).isDirectory()) {
+        getFilesRecursively(absolute);
+      } else {
+        files.push(absolute);
+      }
+    }
+  };
+
+  getFilesRecursively(source);
+
+  const match = RegExp('^[^.]+.js|jsx$');
+
+  return files
+    .filter((file) => file.match(match))
+    .forEach((file) => {
+      const filePath = (source, file);
+
+      if (file.indexOf('/pages/api/') > -1) {
+        const newFilename = file.split('.');
+        const transformed = newFilename[0] + '.ts';
+        renameSync(filePath, transformed);
+      } else {
+        const newFilename = file.split('.');
+        const transformed = newFilename[0] + '.tsx';
+        renameSync(filePath, transformed);
+      }
+    });
 }
 
 function install(root, useYarn, dependancies) {
   return new Promise((resolve, reject) => {
     let command;
-    let args;
+    let args = [];
 
     if (useYarn) {
       command = 'yarn';
